8c766018de25233c5478e416623620a66c46411a
andreluislima <andrelimag89@gmail.com>
2025-05-22 14:43:39 -0300

    feat: footer add

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -2,13 +2,14 @@ import { Component } from '@angular/core';
 import { MenubarBasicDemo } from "./components/navbar/navbar.component";
 import { IndexComponent } from "./pages/index/index/index.component";
 import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
+import { FooterComponent } from "./components/footer/footer.component";
  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [MenubarBasicDemo, IndexComponent, HeroSectionComponent]
+  imports: [MenubarBasicDemo, IndexComponent, HeroSectionComponent, FooterComponent]
 })
 export class AppComponent {
   title = 'projeto';
