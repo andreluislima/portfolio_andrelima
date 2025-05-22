a2a24b296e9fedd2bc2386a94e1e00ee7be76455
andreluislima <andrelimag89@gmail.com>
2025-05-22 14:03:24 -0300

    feat: main e hero add.

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { MenubarBasicDemo } from "./components/navbar/navbar.component";
 import { IndexComponent } from "./pages/index/index/index.component";
+import { HeroSectionComponent } from "./components/hero-section/hero-section.component";
  
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
   standalone: true,
-  imports: [MenubarBasicDemo, IndexComponent]
+  imports: [MenubarBasicDemo, IndexComponent, HeroSectionComponent]
 })
 export class AppComponent {
   title = 'projeto';
---
+++ projeto/src/app/components/hero-section/hero-section.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-section',
+  imports: [],
+  templateUrl: './hero-section.component.html',
+  styleUrl: './hero-section.component.scss'
+})
+export class HeroSectionComponent {
+  title="Sobre Mim"
+  predescription="Meu nome é André Lima e ajudo empresas a crescer no ambiente digital"
+
+  description="Tenho paixão por design e desenvolvimento, criando soluções digitais modernas, eficientes e escaláveis que fazem as marcas se destacarem. Vamos construir algo incrível juntos!"
+
+}
