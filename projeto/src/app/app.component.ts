160e1b87ce87e123e3a75ba7b98fd5d8113a94a8
andreluislima <andrelimag89@gmail.com>
2025-05-27 01:24:51 -0300

    feat: index

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
 import { FooterComponent } from "./components/footer/footer.component";
 import { RouterOutlet } from '@angular/router';
-import { MenubarBasicDemo } from "./components/navbar/navbar.component"; 
+import { MenubarBasicDemo } from "./components/navbar/navbar.component";
+import { IndexComponent } from "./pages/index/index.component"; 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [FooterComponent, RouterOutlet, MenubarBasicDemo]
+  imports: [FooterComponent, RouterOutlet, MenubarBasicDemo, IndexComponent]
 })
 
 export class AppComponent {
