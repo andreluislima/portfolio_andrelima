09124c0a80c680394eb98b87a52a560d261cff69
andecodelima <andrelimag89@gmail.com>
2025-06-05 16:14:47 -0300

    feat: footer e index refatorados

--- projeto/src/app/app.component.ts
+++ projeto/src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenubarBasicDemo } from "./components/navbar/navbar.component";
+import { FooterComponent } from "./components/footer/footer.component";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
   standalone: true,
-  imports: [RouterOutlet, MenubarBasicDemo]
+  imports: [RouterOutlet, MenubarBasicDemo, FooterComponent]
 })
 
 export class AppComponent {
--- projeto/src/app/components/footer/footer.component.ts
+++ projeto/src/app/components/footer/footer.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [CommonModule,RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  footer = ['home', 'resume', 'project', 'contact'];
+
+  nome = "Lima Desenvolvimentos";
+  currentYear: number = new Date().getFullYear();
 
 }
