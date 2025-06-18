2d748e6052b436505c7ba1a576c9af8642ba2ee7
andecodelima <andrelimag89@gmail.com>
2025-06-18 13:45:12 -0300

    feat(resume): add content

--- projeto/src/app/pages/resume/resume.component.ts
+++ projeto/src/app/pages/resume/resume.component.ts
@@ -14,6 +14,63 @@ export class ResumeComponent implements OnInit{
   subtitle=["experience", "education","languages", "Programming Languages", "professional skills"]
   btn="dowload resume"
 
+  experiences = [
+    {
+      "function":"Desenvolvedor Full Stack",
+      "employer":"Ilha Service",
+      "city":"Rio de Janeiro",
+      "client": "INT (Instituto Nacional de Tecnologia",
+      "dateStart":"2022",
+      "dateEnd":"Atualmente",
+      "description":`Responsável técnico pelo sistema principal da instituição.<br>
+                    <strong>Destaque</strong>: Implantação do Git e sistema de versionamento automatizado.
+                    Desenvolvimento com Java, Angular, TypeScript, PostgreSQL e Figma.`
+    },
+
+    {
+      "function":"Analista de suporte",
+      "employer":"CertiRio",
+      "city":"Rio de Janeiro",
+      "client": "INT (Instituto Nacional de Tecnologia",
+      "dateStart":"2019",
+      "dateEnd":"2022",
+      "description":`Atendimento técnico, infraestrutura de TI e certificação digital. <br>
+                     <strong>Destaque</strong>: Implementação de redundância de rede para evitar
+                      indisponibilidades.`
+    }
+  ]
+
+
+  educations = [
+    {
+      "course":"Curso de Programação Full Stack",
+      "school":"Step computer academy",
+      "location":"Rio de Janeiro",
+      "degree":"Especialização",
+      "dateStart":"2022",
+      "dateEnd":"2025"
+    },
+
+    {
+      "course":"Análise em Desenvolvimento de Sistemas",
+      "school":"Unopar",
+      "location":"Rio de Janeiro",
+      "degree":"Graduação",
+      "dateStart":"2018",
+      "dateEnd":"2021"
+    },
+
+    {
+      "course":"Técnico em informática",
+      "school":"Senac,",
+      "location":"Rio de Janeiro",
+      "degree":"Técnico",
+      "dateStart":"2015",
+      "dateEnd":"2015"
+    },
+
+  ]
+
   experience = resume.experience;
   education = resume.education;
   languages = resume.languages;
