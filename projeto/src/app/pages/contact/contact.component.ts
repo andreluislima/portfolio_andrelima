25f16eab54fa71962e45f2f879df18b60652e732
andreluislima <andrelimag89@gmail.com>
2025-06-22 00:44:31 -0300

    feat: start contacts

--- projeto/src/app/pages/contact/contact.component.ts
+++ projeto/src/app/pages/contact/contact.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [],
+  imports: [HeroSectionComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
 export class ContactComponent {
 
+  title_session = "Contact"
 }
