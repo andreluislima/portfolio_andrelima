dcef4cc27f97dfcb8896b21abeedd6005473bb13
andreluislima <andrelimag89@gmail.com>
2025-06-21 22:26:14 -0300

    feat: add content in project

--- projeto/src/app/pages/project/project.component.ts
+++ projeto/src/app/pages/project/project.component.ts
@@ -1,12 +1,37 @@
 import { Component } from '@angular/core';
 import { HeroSectionComponent } from "../../components/hero-section/hero-section.component";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-project',
-  imports: [HeroSectionComponent],
+  imports: [HeroSectionComponent, CommonModule],
   templateUrl: './project.component.html',
   styleUrl: './project.component.scss'
 })
 export class ProjectComponent {
 
+  title = "Projects"
+
+  projects = [
+    {
+      "title":"Restaurante Brazucas",
+      "date":"04/2023",
+      "description":"Desenvolvimento de uma landing page moderna e responsiva para o restaurante Brazucas, especializado em culinária brasileira. O projeto teve como foco destacar a identidade visual brasileira, promover os pratos típicos da casa e facilitar o contato com os clientes por meio de uma navegação simples e intuitiva.",
+      "stacks":"HTML5, CSS3, Bootstrap",
+      "link_projeto":"https://brazucas-restaurante.netlify.app/",
+      "img":"assets/projects/brazucas.png"
+
+    },
+
+    {
+      "title": "Portfólio Profissional",
+      "date": "06/2025",
+      "description": "Criação de um site pessoal com foco em apresentação profissional. O objetivo foi destacar minhas principais habilidades como desenvolvedor Full Stack, exibir projetos realizados e facilitar o contato com recrutadores e clientes. O layout foi pensado para ser moderno, responsivo e de fácil navegação, proporcionando uma boa experiência ao usuário.",
+      "stacks": "HTML5, CSS3, JavaScript, Bootstrap",
+      "link_projeto": "https://portifolioandrelima.netlify.app/",
+      "img":"assets/projects/portifolio.png"
+    }
+
+
+  ]
 }
