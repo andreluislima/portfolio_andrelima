294bc58a651abe4dde10a149571d12cbcb22c803
andecodelima <andrelimag89@gmail.com>
2025-07-08 15:56:22 -0300

    ajustes cors

--- backend/java/api-form-email/src/main/java/net/api/mail/Cors.java
+++ backend/java/api-form-email/src/main/java/net/api/mail/Cors.java
@@ -14,12 +14,16 @@ public class Cors {
 	@Bean
 	public CorsConfigurationSource corsConfigurationSource() {
 	    CorsConfiguration configuration = new CorsConfiguration();
+	    
 	    configuration.setAllowedOrigins(List.of(
 	    		"http://localhost:4200",
 	    		"https://andreluislima.github.io" 
 	    ));
 	    configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
 	    configuration.setAllowedHeaders(List.of("*"));
+	    configuration.setAllowCredentials(true);
+	    
+	    configuration.setExposedHeaders(List.of("Access-Control-Allow-Origin"));
 
 	    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 	    source.registerCorsConfiguration("/**", configuration);
