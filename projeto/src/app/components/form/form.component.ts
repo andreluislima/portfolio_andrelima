14baa30750f4f495533cf7294703a1b2f673ec30
andreluislima <andrelimag89@gmail.com>
2025-07-08 23:47:08 -0300

    update url render

--- projeto/src/app/components/form/form.component.ts
+++ projeto/src/app/components/form/form.component.ts
@@ -27,10 +27,10 @@ export class FormComponent {
   constructor(private http:HttpClient){}
 
   // Local host: http://localhost:8080/enviar-email
-  // Render: https://mail-api-portifolio-docker-2.onrender.com
+  // Render: https://api-email-sender.onrender.com
   submitForm(){
 
-    this.http.post<{mensagem:string}>('https://mail-api-portifolio-docker-2.onrender.com', this.mensagem)
+    this.http.post<{mensagem:string}>('https://api-email-sender.onrender.com', this.mensagem)
     .subscribe({
       next:res => alert(res.mensagem),
       error:err => alert(err + 'Erro ao enviar a mensagem')
