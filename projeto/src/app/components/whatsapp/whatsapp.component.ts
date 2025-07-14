13314ab3075ee89e559e73c0ec9fa863f985f1ac
andecodelima <andrelimag89@gmail.com>
2025-07-14 16:18:58 -0300

    feat: refat.add whatsapp component

---
+++ projeto/src/app/components/whatsapp/whatsapp.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-whatsapp',
+  imports: [],
+  templateUrl: './whatsapp.component.html',
+  styleUrl: './whatsapp.component.scss'
+})
+export class WhatsappComponent {
+
+}
--- projeto/src/app/pages/contact/contact.component.ts
+++ projeto/src/app/pages/contact/contact.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { FormComponent } from "../../components/form/form.component";
+import { WhatsappComponent } from "../../components/whatsapp/whatsapp.component";
 
 @Component({
   selector: 'app-contact',
-  imports: [FormComponent],
+  imports: [FormComponent, WhatsappComponent],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
