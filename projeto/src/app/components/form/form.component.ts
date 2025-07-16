62e4862c4135e03290fd8743362e30d79777edf2
andecodelima <andrelimag89@gmail.com>
2025-07-16 17:48:11 -0300

    feat(form): ajuste estilo dos alertas

--- projeto/src/app/components/form/form.component.ts
+++ projeto/src/app/components/form/form.component.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, NgForm} from '@angular/forms';
 import { NgxMaskDirective, provideNgxMask} from 'ngx-mask';
 import { HttpClient } from '@angular/common/http';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-form',
   imports: [CommonModule,
             FormsModule,
             NgxMaskDirective,
+            
   ],
   providers: [provideNgxMask()],
   templateUrl: './form.component.html',
@@ -25,23 +27,42 @@ export class FormComponent {
   };
 
   
-  constructor(private http:HttpClient){}
+  constructor(private http:HttpClient, private snackBar:MatSnackBar){}
 
   // LocalHost -> http://localhost:8080/enviar-email
   // Render -> https://api-email-sender.onrender.com/enviar-email
 
-
- submitForm(form: NgForm) {
-    this.http.post<{ mensagem: string }>('https://api-email-sender.onrender.com/enviar-email', this.mensagem)
-      .subscribe({
-        next: res => {
-          alert(res.mensagem);
-          form.reset();  
-        },
-        error: err => alert('Erro ao enviar a mensagem: ' + err.message)
-      });
+   submitForm(form: NgForm) {
+    this.http.post<{ mensagem: string }>('http://localhost:8080/enviar-email', this.mensagem).subscribe({
+      next: ()=> this.snackBar.open('Mensagem Enviada com sucesso!', 'Fechar',{
+        duration:3000,
+        horizontalPosition:'center',
+        verticalPosition:'top',
+        panelClass:['snackbar-success']
+      }),
+      error:() => this.snackBar.open('Erro ao enviar a mensagem.', 'Fechar',{
+        duration:3000,
+        horizontalPosition:'center',
+        verticalPosition:'top',
+        panelClass:['snackbar-error']
+      })
+    });
   }
 }
+
+
+// Antigo 2
+//  submitForm(form: NgForm) {
+//     this.http.post<{ mensagem: string }>('https://api-email-sender.onrender.com/enviar-email', this.mensagem)
+//       .subscribe({
+//         next: res => {
+//           alert(res.mensagem);
+//           form.reset();  
+//         },
+//         error: err => alert('Erro ao enviar a mensagem: ' + err.message)
+//       });
+//   }
+// }
 
 
 // Antigo
